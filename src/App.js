7e5abf4782a749ebeed2672f25db97d495812c2a
patriciaSR <patricia.suarez.psr@gmail.com>
2019-10-16 15:26:31 +0200

    add header, footer and filters components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import './App.css';
 import getPokemons from './services/getData';
+import Header from './components/Header/index';
+import Filters from './components/Filters/index';
+import Footer from './components/Footer/index';
 
 class App extends React.Component {
   constructor(props) {
@@ -9,7 +12,7 @@ class App extends React.Component {
       pokemons: [],
       queryName: ''
     };
-  }
+  };
 
   getPokemonsData() {
     getPokemons()
@@ -26,28 +29,33 @@ class App extends React.Component {
           pokemons: info
         });
       })
-  }
+  };
 
   componentDidMount() {
     this.getPokemonsData();
   };
 
   render() {
-    if (this.state.pokemons.length > 0) {
-      return (
-        <div className="App">
-          {this.state.pokemons[0].name}
-      </div>
-      );
-    } else {
-      return (
-        <div className="App">
-          :)
+    return (
+      <>
+        <Header />
+        <main>
+          <Filters />
+          {(this.state.pokemons.length > 0) ? (
+            <div className="App">
+              {this.state.pokemons[0].name}
+            </div>
+          ) : (
+              <div className="App">
+                :D
         </div>
-      );
-    }
-
-  }
+            )
+          }
+        </main>
+        <Footer />
+      </>
+    )
+  };
 }
 
-export default App;
+  export default App;
---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      Pokemons
+    </header>
+  )
+};
+
+export default Header;
